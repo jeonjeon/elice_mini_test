89c474b5109f070b12f59ddd8c6b04fcebe520f9
jeon <hyungzin@gmail.com>
2021-03-26 18:29:07 +0900

    Initial commit

---
+++ android/app/src/main/java/com/example/elice_mini_test/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.elice_mini_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mini Elice'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('empty APP'),
+        ),
+      ),
+    );
+  }
+}
